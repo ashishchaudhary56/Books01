2582325fcce33375bf7af9266e98251d7339a7f9
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-09-21 22:32:55 +0530

    class books01 created create getter setter for authorname,bookname and isbnnumber

---
+++ src/Books.java
@@ -0,0 +1,35 @@
+public class Books {
+}
+
+class Book {
+    private String bookName;
+    private String authorName;
+    private long ISBNNumber;
+
+    public String getBookName() {
+        return bookname;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public long getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(long ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
+
+
+}
+}
